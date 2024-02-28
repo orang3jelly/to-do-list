51da6824be6527a1a33d3613d6594964ca83e363
Mateusz Nowicki <m.nowicki941@gmail.com>
2024-02-28 21:46:17 +0100

    render function added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 {
-    const init = () => {
+  const tasks = [
+    {
+      content: "zadanie 1",
+      done: false,
+    },
+    {
+      content: "zadanie 2",
+      done: true,
+    },
+  ];
 
-    };
+  const render = () => {
+    let htmlString = "";
 
-    init();
+    for (const task of tasks) {
+      htmlString += `
+            <li>
+                ${task.content}
+            </li>
+        `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
+  };
+
+  init();
 }
